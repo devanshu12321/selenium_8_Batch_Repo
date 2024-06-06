8a4b5c308452832e5f5d1cd56bd868b05d434b94
Devanshu Shrivastava <Devanshu Shrivastava@DESKTOP-57E8I4B>
2024-06-06 09:18:58 +0530

    Sample Test added now

--- selenium_maven_8_30/src/test/java/demo/SampleTest.java
+++ selenium_maven_8_30/src/test/java/demo/SampleTest.java
@@ -6,6 +6,8 @@ public class SampleTest {
 	public void test() {
 		Reporter.log("SampleTest class executed",true);
 		Reporter.log("SampleTest class modifieed",true);
+		Reporter.log("SampleTest class added",true);
+		Reporter.log("SampleTest class is added",true);
 	}
 
 }
