13fede1cb971ffc39b14dda396e232714436f6da
Devanshu Shrivastava <Devanshu Shrivastava@DESKTOP-57E8I4B>
2024-06-13 09:28:24 +0530

    Suffix Test is Added

---
+++ selenium_maven_8_30/src/test/java/TestCase_02_Test.java
@@ -0,0 +1,11 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestCase_02_Test {
+
+	@Test
+	public void demo()
+	{
+		Reporter.log("TestCase_02_Test is executed",true);
+	}
+}
